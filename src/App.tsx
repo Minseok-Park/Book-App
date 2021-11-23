71bfbdf8610265d7c100e8ce5ae8afa4da9052d3
minseok-Park <pms0905@naver.com>
2021-11-23 14:19:17 +0900

    라우팅 & 페이지 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Add from "./pages/Add";
+import Detail from "./pages/Detail";
+import Edit from "./pages/Edit";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Signin from "./pages/Singin";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path="/edit/:id" component={Edit} />
+        <Route exact path="/book/:id" component={Detail} />
+        <Route exact path="/add" component={Add} />
+        <Route path="/signin" component={Signin} />
         <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );

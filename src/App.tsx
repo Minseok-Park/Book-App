f7c992a7599cd9d5dcccb77641a80b35ccc4ee06
minseok-Park <pms0905@naver.com>
2021-11-23 15:07:38 +0900

    redux store 설정하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Add from "./pages/Add";
 import Detail from "./pages/Detail";
 import Edit from "./pages/Edit";
+import Error from "./pages/Error";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import Signin from "./pages/Singin";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/edit/:id" component={Edit} />
-        <Route exact path="/book/:id" component={Detail} />
-        <Route exact path="/add" component={Add} />
-        <Route path="/signin" component={Signin} />
-        <Route exact path="/" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/edit/:id" component={Edit} />
+          <Route path="/book/:id" component={Detail} />
+          <Route path="/add" component={Add} />
+          <Route path="/signin" component={Signin} />
+          <Route exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
